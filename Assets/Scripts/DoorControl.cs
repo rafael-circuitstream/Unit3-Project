80d51a1f37899a1798ecbe9045ecb0c7d12040d7
rafael-circuitstream <rafael@circuitstream.ca>
2024-12-17 20:54:33 -0500

    Lesson 2

---
+++ Assets/Scripts/DoorControl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+    public GameObject door;
+
+    public float timer;
+    private void OnTriggerEnter(Collider other)
+    {
+        //Change door color (visual effects)
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Detect every frame I'm in front of the door
+
+
+        //BY INPUT LOGIC
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            door.SetActive(false);
+        }
+
+        /* BY TIME LOGIC HERE
+        timer += Time.deltaTime;
+
+        if(timer >= 3)
+        {
+            
+        }
+        */
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        timer = 0;
+        door.SetActive(true);
+    }
+}
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    //Directional Inputs
     [SerializeField] private Vector2 lookDirection;
     [SerializeField] private Vector3 moveDirection;
 
+    //Reference to the controller
     [SerializeField] private CharacterController controller;
+    //Reference to the Head/Camera GameObject
     [SerializeField] private Camera head;
 
     [SerializeField] private float mouseSensitivity;
 
+    //Movement Settings
     [SerializeField] private float movementSpeed;
-    // Start is called before the first frame update
+
+    //Gravity and Jumping Settings
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravityForce;
+
+    [SerializeField] private LayerMask groundLayer;
+
+    [Header("Shooting Settings")]
+    [SerializeField] private Transform weaponTip;
+    [SerializeField] private Rigidbody projectilePrefab;
+    [SerializeField] private float shootingForce;
     void Start()
     {
-        
+        //Control of Mouse Cursor
+        Cursor.visible = false; //Visibility to hidden
+        Cursor.lockState = CursorLockMode.Locked; //Locked to the center of the screen
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         lookDirection.x += Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
@@ -40,8 +56,45 @@ public class PlayerInput : MonoBehaviour
         Vector3 forwardMovement = moveDirection.z * transform.forward;
         Vector3 sidewaysMovement = moveDirection.x * transform.right;
 
+        Vector3 movementVector = (forwardMovement + sidewaysMovement);
 
-        controller.Move((forwardMovement + sidewaysMovement) * Time.deltaTime * movementSpeed);
- 
+
+        bool isOnGround = Physics.CheckSphere(transform.position, 0.01f, groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            gravityForce = jumpForce;
+            isOnGround = false;
+        }
+
+        controller.Move(movementVector * Time.deltaTime * movementSpeed);
+        
+
+        //Gravity is handled here
+        if (!isOnGround)
+        {
+            //Adding constant gravity every second 
+            gravityForce += -10f * Time.deltaTime;
+            //Apply that movement per second
+            controller.Move(Vector3.up * gravityForce * Time.deltaTime);
+        }
+        else
+        {
+            //Reset gravity once we get to the floor
+            gravityForce = 0;
+        }
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Rigidbody clonedRigidbody = Instantiate(projectilePrefab, weaponTip.position, weaponTip.rotation);
+            clonedRigidbody.AddForce(weaponTip.forward * shootingForce);
+        }
+    }
+
+    //Testing the sphere location
+    private void OnDrawGizmos()
+    {
+        //Drawing a sphere right at the feet of the player
+        Gizmos.DrawSphere(transform.position, 0.1f);
     }
 }
